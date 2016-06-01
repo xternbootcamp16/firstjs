98e1cdda084e0771582fa216621cdce79a28165c
Dave Strus <dave@getfretless.com>
2016-06-01 11:41:19 -0400

    Wrap everything in an IIFE.

--- js/app.js
+++ js/app.js
@@ -1,44 +1,47 @@
 $(document).foundation();
 
-var buildColorDiv = function(color) {
-  if (color === undefined) {
-    color = 'red';
-  }
-  return '<div style="height: 100px; width: 100px; background-color: ' + color + '"></div>'
-};
+(function () {
 
-var buildList = function(listValues) {
-  var dl = document.createElement('dl');
-  dl.style.border = '1px solid #ff0033';
-  dl.appendChild(buildListItem('Name', listValues.name));
-  dl.appendChild(buildListItem('Hair Color', listValues.hairColor));
-  dl.appendChild(buildListItem('Age', listValues.age));
-  dl.appendChild(buildListItem('Birthplace', listValues.birthplace));
-  return dl;
-};
-
-var buildListItem = function(term, definition) {
-  var li = document.createElement('li');
-  var dt = document.createElement('dt');
-  var dd = document.createElement('dd');
-  dt.innerHTML = term;
-  dd.innerHTML = definition;
-  li.appendChild(dt);
-  li.appendChild(dd);
-  return li;
-};
-
-var addValuesToDetails = function(ev) {
-  ev.preventDefault();
-  var details = document.querySelector('.details');
-  var formValues = {
-    name: this.name.value,
-    age: this.age.value,
-    birthplace: this.birthplace.value,
-    hairColor: buildColorDiv(this.hairColor.value)
+  var buildColorDiv = function(color) {
+    if (color === undefined) {
+      color = 'red';
+    }
+    return '<div style="height: 100px; width: 100px; background-color: ' + color + '"></div>'
   };
 
-  details.appendChild(buildList(formValues));
-};
+  var buildList = function(listValues) {
+    var dl = document.createElement('dl');
+    dl.style.border = '1px solid #ff0033';
+    dl.appendChild(buildListItem('Name', listValues.name));
+    dl.appendChild(buildListItem('Hair Color', listValues.hairColor));
+    dl.appendChild(buildListItem('Age', listValues.age));
+    dl.appendChild(buildListItem('Birthplace', listValues.birthplace));
+    return dl;
+  };
 
-document.querySelector('form').onsubmit = addValuesToDetails;
+  var buildListItem = function(term, definition) {
+    var li = document.createElement('li');
+    var dt = document.createElement('dt');
+    var dd = document.createElement('dd');
+    dt.innerHTML = term;
+    dd.innerHTML = definition;
+    li.appendChild(dt);
+    li.appendChild(dd);
+    return li;
+  };
+
+  var addValuesToDetails = function(ev) {
+    ev.preventDefault();
+    var details = document.querySelector('.details');
+    var formValues = {
+      name: this.name.value,
+      age: this.age.value,
+      birthplace: this.birthplace.value,
+      hairColor: buildColorDiv(this.hairColor.value)
+    };
+
+    details.appendChild(buildList(formValues));
+  };
+
+  document.querySelector('form').onsubmit = addValuesToDetails;
+})();
